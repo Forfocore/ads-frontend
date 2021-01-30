e60d816e31bfa1ebfd2a52f52a675b9de915bfd5
Forfocore <kirill.alex88@gmail.com>
2021-01-30 11:48:37 +0500

    New

--- script.js
+++ script.js
@@ -12,5 +12,28 @@ $('#close').click(function() {
 
 });
 
+$('#new').click(function() {
+
+var ad={
+    text: $('textarea[name="text"]').val(),
+    contactName: $('input[name="name"]').val(),
+    contactPhone: $('input[name="phone"]').val()
+}
+
+
+var adHTML=
+"<div class='Content__box'>" +
+"<div class='Box__info'>"+ad.text+"</div>" +
+"<div class='Box__name'>"+ad.contactName+"</div>" +
+"<div class='Box__phone'>"+ad.contactPhone+"</div>" +
+"</div>";
+
+$('#content').append(adHTML);
+$('#modal').hide();
+
+});
+
+var arrayAd = Array();
+
 
 });
