47cead1a9ec0935c7007b7245403f0e1ca78fa30
Forfocore <kirill.alex88@gmail.com>
2021-02-13 02:06:45 +0500

    Save

--- script.js
+++ script.js
@@ -1,39 +1,60 @@
 $(document).ready(function() {
-    $('#modal').hide();
-$('#add').click(function() {
-    $('#modal').show();
+
+    // Основные переменные приложения.
+    var arrayAds = Array(); // Массив объявлений.
+
+    $('#add').click(function() {
+        $('#modal').show();
+    });
+
+    $('#close').click(function() {
+        $('#modal').hide();
+    });
 
 
-});
+    // Добавляем новое обявление.
+    $('#new').click(function() {
+        var ad = {
+            text: $('textarea[name="text"]').val(),
+            contactName: $('input[name="name"]').val(),
+            contactPhone: $('input[name="phone"]').val()
+        }
 
-$('#close').click(function() {
-    $('#modal').hide();
+        arrayAds.push(ad);
+
+        console.log(arrayAds);
+        
+        // Отрисовка новых объявлений.
+        renderAds();
+
+        // Скрыть модальное окно.
+        $('#modal').hide();
+
+        $('textarea[name="text"]').val('');
+        $('input[name="name"]').val('');
+        $('input[name="phone"]').val('');
+    });
 
 
-});
-
-$('#new').click(function() {
-
-var ad={
-    text: $('textarea[name="text"]').val(),
-    contactName: $('input[name="name"]').val(),
-    contactPhone: $('input[name="phone"]').val()
-}
 
 
-var adHTML=
-"<div class='Content__box'>" +
-"<div class='Box__info'>"+ad.text+"</div>" +
-"<div class='Box__name'>"+ad.contactName+"</div>" +
-"<div class='Box__phone'>"+ad.contactPhone+"</div>" +
-"</div>";
+    // Функция отрисовки новых объявлений.
+    function renderAds() {
 
-$('#content').append(adHTML);
-$('#modal').hide();
+        // Очищаем блок контента.
+        $('#content').html('');
 
-});
-
-var arrayAd = Array();
-
+        // Отрисовываем новые объявления.
+        arrayAds.map(ad => {
+            var adHTML = 
+            "<div class='Content__box'>" +
+                "<div class='Box__info'>" + ad.text + "</div>" +
+                "<div class='Box__name'>" + ad.contactName + "</div>" +
+                "<div class='Box__phone'>" + ad.contactPhone + "</div>" +
+            "</div>";
+        
+            $('#content').append(adHTML);
+        });
+    }
 
 });
